5cee7e683790e9336ca647564c8f0ac03ac07993
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-09-18 16:18:33 -0400

    JS fixes
    Fixing CORS issue and capital letters no logger break API.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = Vue.createApp({
     methods: {
         getPokemon(pokemon) {
             axios
-            .get("https://cors-anywhere.herokuapp.com/pokeapi.co/api/v2/pokemon/" + pokemon)
+            .get("https://pokeapi.co/api/v2/pokemon/" + pokemon.toLowerCase())
             .then(response => (this.ability2 = "Ability: " + response.data.abilities[1].ability.name, this.ability1 = "Ability: " + response.data.abilities[0].ability.name, this.imgURLback = response.data.sprites.back_default, this.imgURL = response.data.sprites.front_default, this.poke = response.data.species.name, this.baseHP = "HP: " + response.data.stats[0].base_stat, this.defense = "Defense: " + response.data.stats[2].base_stat, this.attack = "Attack: " + response.data.stats[1].base_stat, this.spAttack = "Special Attack: " + response.data.stats[3].base_stat, this.spDefense = "Special Defense: " + response.data.stats[4].base_stat, this.speed = "Speed: " + response.data.stats[5].base_stat, this.show = true, console.log(response)))
             .catch(e => (console.log(e)))
         }
