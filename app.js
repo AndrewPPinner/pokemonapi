103484894e9d83babf40f5ee4eb783a6028664b6
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-09-18 15:37:30 -0400

    Updating Stats logic and sprites

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ const app = Vue.createApp({
             speed: null,
             poke: null,
             show: null,
-            imgURL: null
+            imgURL: null,
+            imgURLback: null,
+            ability1: null,
+            ability2: null,
+            hidden: null
         }
     },
     methods: {
         getPokemon(pokemon) {
             axios
             .get("https://cors-anywhere.herokuapp.com/pokeapi.co/api/v2/pokemon/" + pokemon)
-            .then(response => (this.imgURL = response.data.sprites.front_default, this.poke = response.data.species.name, this.baseHP = "HP: " + response.data.stats[0].base_stat, this.defense = "Defense: " + response.data.stats[2].base_stat, this.attack = "Attack: " + response.data.stats[1].base_stat, this.spAttack = "Special Attack: " + response.data.stats[3].base_stat, this.spDefense = "Special Defense: " + response.data.stats[4].base_stat, this.speed = "Speed: " + response.data.stats[5].base_stat, this.show = true, console.log(response)))
+            .then(response => (this.ability2 = "Ability: " + response.data.abilities[1].ability.name, this.ability1 = "Ability: " + response.data.abilities[0].ability.name, this.imgURLback = response.data.sprites.back_default, this.imgURL = response.data.sprites.front_default, this.poke = response.data.species.name, this.baseHP = "HP: " + response.data.stats[0].base_stat, this.defense = "Defense: " + response.data.stats[2].base_stat, this.attack = "Attack: " + response.data.stats[1].base_stat, this.spAttack = "Special Attack: " + response.data.stats[3].base_stat, this.spDefense = "Special Defense: " + response.data.stats[4].base_stat, this.speed = "Speed: " + response.data.stats[5].base_stat, this.show = true, console.log(response)))
             .catch(e => (console.log(e)))
         }
     }
